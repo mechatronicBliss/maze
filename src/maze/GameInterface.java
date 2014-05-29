9e42add18c105aede7329008e3322f75d086c636
boredj <le.jason@gmx.com>
2014-05-29 22:33:03 +1000

    changed aas a result of the merge error

--- src/maze/GameInterface.java
+++ src/maze/GameInterface.java
@@ -5,7 +5,7 @@ import java.awt.event.*;
 import javax.swing.*;
 
 
-public class GameInterface implements ActionListener{
+public class GameInterface extends JComponent implements ActionListener{
 	private int size;
 	private double p;
 	private GameState gameState;
@@ -14,7 +14,9 @@ public class GameInterface implements ActionListener{
     private MazeUi m;
     private int theme;
     private JPanel sideBar;
+    
     private boolean useCollectables;
+    private String victory;
 	public GameInterface() {
 		size = 10;
 		p = 0.25;
@@ -24,6 +26,7 @@ public class GameInterface implements ActionListener{
         this.theme = 1;
 	}
 	private void displayMenu() {
+        this.victory = "Much Victory!";
         this.theme = 1;
         this.size = 10;
         this.p = 0.25;
@@ -34,28 +37,30 @@ public class GameInterface implements ActionListener{
 		JButton startButton = new JButton("Start");
 		startButton.setActionCommand("start");
 		startButton.addActionListener(this);
+		startButton.setFont(new Font("Arial", Font.BOLD, 20));
+		
 		JButton instructions = new JButton("Instructions");
 		instructions.setActionCommand("showInstructions");
 		instructions.addActionListener(this);
-		JButton custom = new JButton("custom");
+		instructions.setFont(new Font("Arial", Font.BOLD, 20));
 		
+		//JButton custom = new JButton("custom");
+		
+		//radio buttons 
 		JRadioButton easy = new JRadioButton("Easy");
 		easy.setActionCommand("e");
 		easy.setSelected(true);
+		easy.setForeground(Color.yellow);
 		easy.addActionListener(this);
 		JRadioButton medium = new JRadioButton("Medium");
 		medium.setActionCommand("m");
+		medium.setForeground(Color.yellow);
 		medium.addActionListener(this);
 		JRadioButton hard = new JRadioButton("Hard");
 		hard.setActionCommand("h");
+		hard.setForeground(Color.yellow);
 		hard.addActionListener(this);
 
-
-
-        /*sideBar = new JPanel(new GridBagLayout());
-        sideBar.setOpaque(false);
-        sideBar.add(exit);    */
-		
 		//group the radios
 		ButtonGroup group = new ButtonGroup();
 		group.add(easy);
@@ -63,18 +68,20 @@ public class GameInterface implements ActionListener{
 		group.add(hard);
 		
 		menuPanel = new JPanel(new GridBagLayout());
-        menuPanel.setBackground(Color.darkGray);
+        menuPanel.setBackground(Color.BLACK);
 		GridBagConstraints c = new GridBagConstraints();
+		
 		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridx = 0;
+		c.gridx = 1;
 		c.gridy = 0;
         menuPanel.add(instructions, c);
 		
 		c.gridx = 1;
+		c.gridy = 6;
         menuPanel.add(startButton, c);
 		
-		c.gridx = 2;
-        menuPanel.add(custom, c);
+		//c.gridx = 2;
+       // menuPanel.add(custom, c);
 		
 		c.gridx = 0;
 		c.gridy = 1;
@@ -83,40 +90,51 @@ public class GameInterface implements ActionListener{
         menuPanel.add(medium, c);
 		c.gridy = 3;
         menuPanel.add(hard, c);
+        
         c.gridx = 2;
-        c.gridy = 2;
+        c.gridy = 3;
 
         JCheckBox collectables = new JCheckBox("Collectables");
         collectables.setActionCommand("collect");
         collectables.addActionListener(this);
+        collectables.setForeground(Color.yellow);
         menuPanel.add(collectables, c);
+       
         c.gridx = 1;
         c.gridy = 1;
-        JRadioButton themeOne = new JRadioButton("IRON MAN!");
+        JRadioButton themeOne = new JRadioButton("Theme 1");
         themeOne.setActionCommand("theme1");
         themeOne.setSelected(true);
         themeOne.addActionListener(this);
+        themeOne.setForeground(Color.yellow);
         JRadioButton themeTwo = new JRadioButton("Theme 2");
         themeTwo.setActionCommand("theme2");
         themeTwo.addActionListener(this);
+        themeTwo.setForeground(Color.yellow);
         JRadioButton themeThree = new JRadioButton("Theme 3");
         themeThree.setActionCommand("theme3");
         themeThree.addActionListener(this);
-        JRadioButton themeFour = new JRadioButton("Space");
+        themeThree.setForeground(Color.yellow);
+        JRadioButton themeFour = new JRadioButton("Theme 4");
         themeFour.setActionCommand("theme4");
+        themeFour.setForeground(Color.yellow);
         themeFour.addActionListener(this);
-        
+        JLabel themeLabel = new JLabel();
+        themeLabel.setText("Themes");
+        themeLabel.setForeground(Color.yellow);
         ButtonGroup themes = new ButtonGroup();
         themes.add(themeOne);
         themes.add(themeTwo);
         themes.add(themeThree);
         themes.add(themeFour);
-        menuPanel.add(themeOne, c);
+        menuPanel.add(themeLabel, c);
         c.gridy = 2;
-        menuPanel.add(themeTwo, c);
+        menuPanel.add(themeOne, c);
         c.gridy = 3;
-        menuPanel.add(themeThree, c);
+        menuPanel.add(themeTwo, c);
         c.gridy = 4;
+        menuPanel.add(themeThree, c);
+        c.gridy = 5;
         menuPanel.add(themeFour, c);
 		gameFrame.add(menuPanel);
 		gameFrame.setVisible(true);
@@ -155,6 +173,7 @@ public class GameInterface implements ActionListener{
         }
         else if(e.getActionCommand().equals("theme1")) {
             theme = 1;
+            this.victory = "Much Victory!";
         }
         else if(e.getActionCommand().equals("theme2")) {
             theme = 2;
@@ -164,6 +183,7 @@ public class GameInterface implements ActionListener{
         }
         else if(e.getActionCommand().equals("theme4")) {
             theme = 4;
+            this.victory = "The humans are dead";
         }
 	}
 	public int getDifficulty() {
@@ -183,9 +203,12 @@ public class GameInterface implements ActionListener{
     public void restart() {
         gameFrame.setVisible(false);
         gameFrame = new JFrame();
+        this.victory = "Much Victory!";
         displayMenu();
     }
-    private void openCustom() {
 
+    public String getVictoryMessage() {
+        return victory;
     }
+    
 }
